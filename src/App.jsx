aa223b496090d5d81c6e200738b1d1cc613656bf
silvajuniordorival-eng <silva.junior.dorival@escola.pr.gov.br>
2026-03-03 20:23:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import functionalComponent from './component/functionalComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Alo mundo</h1>
+     <functionalComponent/>
+
+
+
+
+
+
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/component/functionalComponent.jsx
@@ -0,0 +1,10 @@
+function functionalComponent(){
+    return(
+        <div>
+            <h2>Primeiro Componente</h2>
+            
+        </div>
+    )
+}
+
+export default functionalComponent
