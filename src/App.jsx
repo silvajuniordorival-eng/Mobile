7437f4177301d1d31e397f40ae6f0b3c97858eb8
silvajuniordorival-eng <silva.junior.dorival@escola.pr.gov.br>
2026-03-17 20:24:36 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import FunctionalComponent from './Components/FunctionalComponent'
-import FunçoesClasses from './Components/FuncoesClasses'
-import FuncaoProps from './Components/FuncaoProps'
+import Exemplos from './components/Exemplos'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div id="area">
-      <h1>Alo Mundo</h1>
-      <img className='titulo' src="https://github.com/dgouvvea/Img/blob/main/RN/RNc.png?raw=true" alt="Logo React Nativea" />
+      <section id="center">
+        <h1>SegundaApp</h1>
+        <Exemplos/>
 
-      <FunctionalComponent />
-      <FunçoesClasses />
-      <FuncaoProps />
-      
-      <img src="https://github.com/dgouvvea/Img/blob/main/RN/RNr.png?raw=true" alt="" className="rodape" />
-    </div>
+      </section>
+
     </>
   )
 }
---
+++ src/components/Exemplos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exemplos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Exemplos
